5b2737dae1205d8655b9be59af4f411d414dfb3f
YellowGB <yellowchickencorp@gmail.com>
2022-01-17 18:02:19 +0100

    RR-8 17-01-2022 18:00 fin sections localization ressource.blade.php

--- resources/views/ressource.blade.php
+++ resources/views/ressource.blade.php
@@ -41,7 +41,7 @@
         {{-- Fin Activite --}}
         {{-- Début Article --}}
         @case('App\Models\Article')
-            <p>Source : {{ $content->source_url }}</p>
+            <p>{{ __('titles.link.source') }} : {{ $content->source_url }}</p>
             @break
         {{-- Fin Article --}}
         {{-- Début Atelier --}}
@@ -52,60 +52,60 @@
         {{-- Fin Atelier --}}
         {{-- Début Course --}}
         @case('App\Models\Course')
-            <p>Emplacement du fichier : {{ $content->file_uri }}</p>
-            <p>Nom du fichier : {{ $content->file_name }}</p>
+            <p>{{ __('titles.link.uri') }} : {{ $content->file_uri }}</p>
+            <p>{{ __('titles.filename') }} : {{ $content->file_name }}</p>
             @break
         {{-- Fin Course --}}
         {{-- Début Defi --}}
         @case('App\Models\Defi')
-            <h3>Description</h3>
+            <h3>{{ __('titles.content.description') }}</h3>
             <p>{{ $content->description }}</p>
-            <h4>Bonus</h4>
+            <h4>{{ __('titles.content.bonus') }}</h4>
             <p>{{ $content->bonus }}</p>
             @break
         {{-- Fin Defi --}}
         {{-- Début Jeu --}}
         @case('App\Models\Jeu')
-            <h3>Description</h3>
+            <h3>{{ __('titles.content.description') }}</h3>
             <p>{{ $content->description }}</p>
             <p>
-                Démarre le
+                {{ __('titles.content.starting') }}
                 {{ \Carbon\Carbon::parse($content->starting_date)->format('d/m/Y') }}
-                à
+                {{ __('titles.at') }}
                 {{ \Carbon\Carbon::parse($content->starting_date)->format('H:i') }}
             </p>
-            <p>Lien : {{ $content->link }}</p>
+            <p>{{ __('titles.link.link') }} : {{ $content->link }}</p>
             @break
         {{-- Fin Jeu --}}
         {{-- Début Lecture --}}
         @case('App\Models\Lecture')
-            <h2>Titre : {{ $content->title }}</h2>
-            <p>Auteur : {{ $content->author }}</p>
-            <p>Année de parution : {{ $content->year }}</p>
-            <p>Résumé : {{ $content->summary }}</p>
-            <p>Analyse : {{ $content->analysis }}</p>
-            <p>Avis : {{ $content->review }}</p>
+            <h2>{{ __('titles.title') }} : {{ $content->title }}</h2>
+            <p>{{ trans_choice('titles.author', 1) }} : {{ $content->author }}</p>
+            <p>{{ __('titles.content.publication') }} : {{ $content->year }}</p>
+            <p>{{ __('titles.content.summary') }} : {{ $content->summary }}</p>
+            <p>{{ __('titles.content.analysis') }} : {{ $content->analysis }}</p>
+            <p>{{ __('titles.content.review') }} : {{ $content->review }}</p>
             @break
         {{-- Fin Lecture --}}
         {{-- Début Photo --}}
         @case('App\Models\Photo')
-            <p>Emplacement du fichier : {{ $content->file_uri }}</p>
-            <p>Auteur : {{ $content->photo_author }}</p>
-            <p>Légende : {{ $content->legend }}</p>
+            <p>{{ __('titles.link.uri') }} : {{ $content->file_uri }}</p>
+            <p>{{ trans_choice('titles.author', 1) }} : {{ $content->photo_author }}</p>
+            <p>{{ __('titles.content.legend') }} : {{ $content->legend }}</p>
             @break
         {{-- Fin Photo --}}
         {{-- Début Video --}}
         @case('App\Models\Video')
             @if (is_null($content->link))
-                <p>Emplacement du fichier : {{ $content->file_uri }}</p>
+                <p>{{ __('titles.link.uri') }} : {{ $content->file_uri }}</p>
             @else
-                <p>Lien : {{ $content->link }}</p>
+                <p>{{ __('titles.link.link') }} : {{ $content->link }}</p>
             @endif
-            <p>Légende : {{ $content->legend }}</p>
+            <p>{{ __('titles.content.legend') }} : {{ $content->legend }}</p>
             @break
         {{-- Fin Video --}}
         @default
-            <h1>Aucun contenu trouvé pour cette ressource</h1>
+            <h1>{{ __('titles.content.none') }}</h1>
             
     @endswitch
     {{-- Fin du contenu --}}
