9cf81e63b57936903b2dcd3a2e6150d5943c63a2
Tristan69680 <johndoe@example.com>
2022-01-23 12:55:18 +0100

    RR-54 23/01/2022 12:55 Add style catalogue pagewithout JavaScript

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script defer src="theme.js"></script>
-        <link rel="stylesheet" type="text/css" href="{{ url('./style.css') }}">
+        <link href="/css/style.css" rel="stylesheet">
         <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css" />
         <link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet">
         
