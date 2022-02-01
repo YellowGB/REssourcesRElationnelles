38b19407cb4b731995af58edbd2a32320af7d733
YellowGB <yellowchickencorp@gmail.com>
2022-02-02 00:29:05 +0100

    v0.6.7-alpha

--- config/constants.php
+++ config/constants.php
@@ -4,19 +4,19 @@ use App\Enums\UserPermission;
 
 return [
     'permissions' => [
-        UserPermission::CreateRessources->value => 0,
-        UserPermission::PublishRessources->value => 0,
-        UserPermission::UpdateRessourcesSelf->value => 0,
-        UserPermission::UpdateRessourcesOthers->value => 0,
-        UserPermission::DeleteRessourcesSelf->value => 0,
-        UserPermission::DeleteRessourcesOthers->value => 0,
-        UserPermission::CreateUsers->value => 0,
-        UserPermission::UpdateUsers->value => 0,
-        UserPermission::DeleteUsers->value => 0,
-        UserPermission::RemoveUsers->value => 0, // Supprimer définitivement
-        UserPermission::CreateCategories->value => 0,
-        UserPermission::UpdateCategories->value => 0,
-        UserPermission::DeleteCategories->value => 0,
-        UserPermission::AccessAdmin->value => 0,
+        UserPermission::CreateRessources->value         => 0,
+        UserPermission::PublishRessources->value        => 0,
+        UserPermission::UpdateRessourcesSelf->value     => 0,
+        UserPermission::UpdateRessourcesOthers->value   => 0,
+        UserPermission::DeleteRessourcesSelf->value     => 0,
+        UserPermission::DeleteRessourcesOthers->value   => 0,
+        UserPermission::CreateUsers->value              => 0,
+        UserPermission::UpdateUsers->value              => 0,
+        UserPermission::DeleteUsers->value              => 0,
+        UserPermission::RemoveUsers->value              => 0, // Supprimer définitivement
+        UserPermission::CreateCategories->value         => 0,
+        UserPermission::UpdateCategories->value         => 0,
+        UserPermission::DeleteCategories->value         => 0,
+        UserPermission::AccessAdmin->value              => 0,
     ],
 ];
