cc32d5c113ea44812eca116178c402c6812cf50b
YellowGB <yellowchickencorp@gmail.com>
2022-02-05 01:34:14 +0100

    RR-21 05-02-2022 comment reports, event, listener and notification

---
+++ app/Http/Controllers/CommentaireController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Events\CommentReported;
+use App\Models\Commentaire;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+
+class CommentaireController extends Controller
+{
+    public function report($id) {
+
+        $commentaire  = Commentaire::findOrFail($id);
+        
+        $commentaire->reports++;
+        $commentaire->update();
+
+        event(new CommentReported($commentaire));
+
+        // On rafraichit simplement la page pour le moment (AJAX à l'avenir)
+        return redirect()->route('ressources.show', ['id' => $commentaire->ressource_id]);
+    }
+}
---
+++ app/Listeners/SendCommentReportNotification.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Enums\UserRole;
+use App\Events\CommentReported;
+use App\Notifications\CommentReportedNotification;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendCommentReportNotification
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\CommentReported  $event
+     * @return void
+     */
+    public function handle(CommentReported $event)
+    {
+        $role = Role::where('name', UserRole::Moderator->value)->firstOrFail();
+        $moderators = User::where('role_id', $role->id)->get();
+
+        foreach ($moderators as $moderator) {
+            $moderator->notify(new CommentReportedNotification($event->commentaire));
+        }
+    }
+}
