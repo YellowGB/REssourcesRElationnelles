51d0ed4035f979e9f3180dca9424d9404ae28da4
YellowGB <yellowchickencorp@gmail.com>
2022-02-17 18:08:27 +0100

    v0.8.4-alpha

--- config/app.php
+++ config/app.php
@@ -25,7 +25,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => env('APP_VERSION', '0.8.3-alpha'),
+    'version' => env('APP_VERSION', '0.8.4-alpha'),
 
     /*
     |--------------------------------------------------------------------------
