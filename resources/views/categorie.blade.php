bd8ac1e8c28295b8be100ef6a59ac9f89948200c
Tristan SANCHEZ <tsanchez@silverprod.fr>
2022-02-17 17:55:32 +0100

    RR-20 adding frontend on categories page

--- resources/views/categorie.blade.php
+++ resources/views/categorie.blade.php
@@ -2,10 +2,10 @@
 
     <x-page-header heading="{{ __('titles.section.category') }}" />
 
-    <div class="flex flex-col lg:w-2/3 place-items-center">
+    <div class="flex flex-col items-center justify-between">
 
         @foreach ($categories as $categorie)
-            <p class="bg-light">
+            <p class="max-w-md lg:max-w-4xl px-10 py-6 mx-auto my-4 bg-violet-lightest dark:bg-violet-light rounded-lg shadow-md">
                 @lang('titles.category.' . $categorie->name)
             </p>
         @endforeach
