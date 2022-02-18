ecf30fb5a43abe0ce837bdf4b6287ceb6086122d
Tristan SANCHEZ <tsanchez@silverprod.fr>
2022-02-18 16:32:55 +0100

    RR-20 update frontend page categories with responsive

--- resources/views/categorie.blade.php
+++ resources/views/categorie.blade.php
@@ -2,22 +2,22 @@
 
     <x-page-header heading="{{ __('titles.section.category') }}" />
 
-    <div class="flex flex-col items-center justify-between">
+    <div class="grid grid-cols-1 sm:grid-cols-2 sm:justify-center md:grid-cols-3 items-center justify-between">
 
         @foreach ($categories as $categorie)
-            <p class="max-w-md lg:max-w-4xl px-10 py-6 mx-auto my-4 bg-violet-lightest dark:bg-violet-light rounded-lg shadow-md">
+            <p class="text-clip overflow-hidden max-w-md lg:max-w-4xl w-8/12 p-5 mx-auto my-2 lg:mt-6 ml-4 md:ml-5 md:my-1 md:mt-2 bg-violet-lightest dark:bg-violet-light rounded-lg shadow-md">
                 @lang('titles.category.' . $categorie->name)
             </p>
         @endforeach
-
-        <form action="{{ route('categories.store') }}" method="post" enctype="multipart/form-data">
-            @csrf
-                <input type="text" name="name">
-                <button type="submit" class="mt-4">
-                    {{ __('titles.btn.create') }}
-                </button>
-        </form>
     </div>
+    <div class="flex justify-center my-2 mt-4">
+    <form action="{{ route('categories.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+            <input type="text" name="name" class="mt-4 ml-4">
+            <button type="submit" class="w-40 mt-4 ml-4">
+                {{ __('titles.btn.create') }}
+            </button>
+    </form>
 
 
 </x-app-layout>
