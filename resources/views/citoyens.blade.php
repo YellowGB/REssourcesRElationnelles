49ee372263d63470b02022d6aad675e939b7991c
Tristan SANCHEZ <tsanchez@silverprod.fr>
2022-02-21 19:07:22 +0100

    RR-23 Responsive citizens page and categories page

--- resources/views/citoyens.blade.php
+++ resources/views/citoyens.blade.php
@@ -1,18 +1,20 @@
 <x-app-layout>
     <x-page-header heading="{{ __('titles.section.users') }}" />
     @foreach ($citoyens as $citoyen)
-    <p class="max-w-md lg:max-w-1xl px-10 py-5 mx-auto my-2 ml-4 mr-4 bg-violet-lightest dark:bg-violet-light rounded-lg shadow-md">
-        {{ $citoyen->name }}
-        {{ $citoyen->firstname }}
-        <div class="flex">
-            <form action="{{ route('citoyens.update', ['id' => $citoyen->id]) }}" method="post" enctype="multipart/form-data">
-            @csrf
-                <button type="submit" class="ml-4 mr-2 justify-around w-32">
-                    {{ is_null($citoyen->suspended_at) ? __('titles.user.suspend') : __('titles.user.unsuspend')  }}
-                </button>
-            </form>
+    <div class="grid grid-cols-1 sm:grid-cols-2 sm:justify-center md:grid-cols-3 items-center justify-between">
+        <p class="text-clip overflow-hidden max-w-md px-10 py-5 my-2 ml-2 mr-3 bg-violet-lightest dark:bg-violet-light rounded-lg shadow-md">
+            {{ $citoyen->name }}
+            {{ $citoyen->firstname }}
+            <div class="flex">
+                <form action="{{ route('citoyens.update', ['id' => $citoyen->id]) }}" method="post" enctype="multipart/form-data">
+                @csrf
+                    <button type="submit" class="ml-4 mr-2 justify-around w-32">
+                        {{ is_null($citoyen->suspended_at) ? __('titles.user.suspend') : __('titles.user.unsuspend')  }}
+                    </button>
+                </form>
 
-        </div>
-    </p>
+            </div>
+        </p>
+    </div>
     @endforeach
 </x-app-layout>
