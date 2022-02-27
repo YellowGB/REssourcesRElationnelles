1e2dd2437f676261241cab6171bb07a6f0187b81
Hitoraya <bastien.herry@outlook.com>
2022-02-27 17:59:25 +0100

    RR-26 moderation commentaires front

--- resources/views/commentaires-moderation.blade.php
+++ resources/views/commentaires-moderation.blade.php
@@ -1,18 +1,23 @@
 <x-app-layout>
 
-    <h2>{{ __('titles.section.comments') . ' ' . __('titles.comment.reported') }}</h2>
+    <x-page-header heading="{{ __('titles.section.comments') . ' ' . __('titles.comment.reported') }}" />
     @foreach ($commentaires as $commentaire)
     {{-- {{ dd($commentaire->user_id); }} --}}
         <div 
         onclick="location.href='{{ route('resources.show', ['id' => $commentaire->ressource_id]) }}'"
         class="max-w-md lg:max-w-4xl px-10 py-6 mx-auto my-4 bg-violet-lightest dark:bg-violet-light rounded-lg shadow-md cursor-pointer"
         >
-            <p>{{ $commentaire->content }}</p>
-            <h2>{{ __('titles.by')  }} : {{ $commentaire->user->firstname }} {{ $commentaire->user->name }}</h2>
-            <h2>{{ __('titles.section.resource') }} : {{ $commentaire->ressource->title }}</h2>
+            <h2>{{ $commentaire->content }}</h2>
+            <h3>{{ __('titles.by')  }} : {{ $commentaire->user->firstname }} {{ $commentaire->user->name }}</h3>
+            <p>{{ __('titles.section.resource') }} : {{ $commentaire->ressource->title }}</p>
             <p>{{ __(trans_choice('titles.comment.reports', $commentaire->reports)) }} : {{ $commentaire->reports }}</p>
-            <form action="{{ route('comment.moderation', $commentaire->id) }}">
-                <input class="cursor-pointer" type="submit" value="{{ __('titles.moderation.commentaire') }}" />
+            <form action="{{ route('comment.ignorer', $commentaire->id) }}" method='POST'>
+                @csrf
+                <input type="submit" value="{{ __('titles.comment.ignore') }}" />
+            </form>
+            <form action="{{ route('comment.supprimer', $commentaire->id) }}" method='POST'>
+                @csrf
+                <input type="submit" value="{{ __('titles.comment.delete') }}" />
             </form>
         </div>
     @endforeach
