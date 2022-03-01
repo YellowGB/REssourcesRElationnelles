5075d20ba064fcbe078c9baeb2cc4d4b34ea928e
YellowGB <yellowchickencorp@gmail.com>
2022-03-02 00:27:27 +0100

    RR-21 update view creation resource +  triptyque + creation diptyque

--- resources/views/layouts/triptyque.blade.php
+++ resources/views/layouts/triptyque.blade.php
@@ -1,4 +1,4 @@
-<div class="grid grid-cols-1 lg:grid-cols-catalogue gap-4 justify-items-stretch items-stretch">
+<div class="grid grid-cols-1 lg:grid-cols-triptyque gap-4 justify-items-stretch items-stretch">
     {{-- Le premier div sert uniquement à conserver l'espace de la première colonne de la grid, donnant la possibilité au prochain div d'être fixed sans casser le layout de la page --}}
     <div>
         {{ $left ?? '' }}
