2819b7c6c779b24f0b72eb67abc89ec3d5cfd6bc
YellowGB <yellowchickencorp@gmail.com>
2022-03-02 00:07:34 +0100

    RR-21 quickfix anchor color darkmode

--- resources/views/components/togglers/lang.blade.php
+++ resources/views/components/togglers/lang.blade.php
@@ -3,7 +3,12 @@
     <ul>
         @foreach(LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
             <li class="ml-2">
-                <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}">
+                <a
+                    rel="alternate"
+                    hreflang="{{ $localeCode }}"
+                    href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}"
+                    class="dark:text-violet-lighter dark:hover:text-violet-lightest"
+                >
                     {{ $properties['native'] }}
                 </a>
             </li>
