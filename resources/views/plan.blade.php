caa36dcc0fe6908c357db6260e970313ced0224f
Tristan SANCHEZ <tsanchez@silverprod.fr>
2022-03-12 17:10:45 +0100

    RR-78 Structure modification and color adding

--- resources/views/plan.blade.php
+++ resources/views/plan.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <div class="text-clip overflow-hidden max-w-4xl px-10 py-6 ml-4 mr-4 mt-4 bg-violet-lightest dark:bg-violet-light rounded-lg shadow-md">
         <h1>Plan du Site</h1>
-        <h2>Accueil<h2>
+        <h2>
+            <div class="text-sky-600">Accueil</div>
+        <h2>
             <ul class="list-disc">
                 <li class="ml-4">
                     Inscription
@@ -12,21 +14,21 @@
                         <li class="ml-4">
                             Tableau de bord
                             <ul class="list-disc">
-                                <li class="ml-4">Tableau de progression</li>
-                                <li class="ml-4">Catégories</li>
-                                <li class="ml-4">Statistiques</li>
+                                <p class="ml-4">Tableau de progression</p>
+                                <p class="ml-4">Catégories</p>
+                                <p class="ml-4">Statistiques</p>
                                 <li class="ml-4">
                                     Utilisateurs
                                     <ul class="list-disc">
-                                        <li class="ml-4">Création de modérateurs / administrateur</li>
+                                        <p class="ml-4">Création de modérateurs / administrateur</li>
+                                        <p class="ml-4">Profil</p>
+                                        <p class="ml-4">Modération de ressources</p>
+                                        <p class="ml-4">Modération de commentaire</p>
                                     </ul>
                                 </li>
-                                <li class="ml-4">Profil</li>
-                                <li class="ml-4">Modération de ressources</li>
-                                <li class="ml-4">Modération de commentaire</li>
+                                <li class="ml-4">Mot de passe oublié</li>
                             </ul>
                         </li>
-                        <li class="ml-4">Mot de passe oublié</li>
                     </ul>
                 </li>
                 <li class="ml-4">
@@ -35,7 +37,7 @@
                         <li class="ml-4">
                             Ressources
                             <ul class="list-disc">
-                                <li class="ml-4">Création / Edition de Ressources</li>
+                                <p class="ml-4">Création / Edition de Ressources</p>
                             </ul>
                         </li>
                     </ul>
