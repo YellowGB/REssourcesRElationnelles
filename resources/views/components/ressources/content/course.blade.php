e01e7a5b10e907fbd9a13288d421d5570f4a7352
YellowGB <yellowchickencorp@gmail.com>
2022-03-14 00:32:59 +0100

    RR-57 vue course + force pdf exemple

--- resources/views/components/ressources/content/course.blade.php
+++ resources/views/components/ressources/content/course.blade.php
@@ -1,5 +1,19 @@
 <div>
-    <form action="{{ route('courses.show', ['id' => $content->id]) }}">
+    {{-- <form action="{{ route('courses.show', ['id' => $content->id]) }}">
         <input type="submit" value="{{ __('titles.readfile') . ' ' . $content->file_name }}" class="cursor-pointer">
-    </form>
+    </form> --}}
+
+    <div id="pdf-viewer"></div>
+
+    <style>
+        .pdfobject-container {
+            height: 100vh;
+        }
+    </style>
+
+    {{-- Documentation : https://pdfobject.com/ --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfobject/2.2.7/pdfobject.min.js"></script>
+    <script>
+        PDFObject.embed("{{ route('courses.show', ['id' => $content->id]) }}", "#pdf-viewer");
+    </script>
 </div>
