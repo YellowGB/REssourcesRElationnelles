85212b4bf23b6c05d4c8ef970b09617d00ccd152
YellowGB <yellowchickencorp@gmail.com>
2022-03-13 23:55:14 +0100

    RR-57 vue atelier

--- resources/views/components/ressources/content/article.blade.php
+++ resources/views/components/ressources/content/article.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <h3 class="font-semibold">{{ __('titles.link.follow') }}</h3>
+    <h3>{{ __('titles.link.follow') }}</h3>
     <div class="bg-gris-normal rounded-xl border-2 border-gris-dark p-4">
         <a href="{{ $content->source_url }}" target="_blank">{{ $content->source_url }}</a>
     </div>
