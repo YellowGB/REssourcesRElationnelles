d426f92056fe9e22d4355e08482602bd3ed74b1b
YellowGB <yellowchickencorp@gmail.com>
2022-03-14 01:29:50 +0100

    RR-57 vue lecture

--- resources/views/components/ressources/content/lecture.blade.php
+++ resources/views/components/ressources/content/lecture.blade.php
@@ -2,7 +2,12 @@
     <h2>{{ __('titles.content.title') }} : {{ $content->title }}</h2>
     <p>{{ trans_choice('titles.author', 1) }} : {{ $content->author }}</p>
     <p>{{ __('titles.content.publication') }} : {{ $content->year }}</p>
-    <p>{{ __('titles.content.summary') }} : {{ $content->summary }}</p>
-    <p>{{ __('titles.content.analysis') }} : {{ $content->analysis }}</p>
-    <p>{{ __('titles.content.review') }} : {{ $content->review }}</p>
+
+    <x-sep-horizontal />
+
+    <div class="flex flex-col gap-4 justify-center items-center text-justify">
+        <p><span class="underline font-semibold">{{ __('titles.content.summary') }}</span> : {{ $content->summary }}</p>
+        <p><span class="underline font-semibold">{{ __('titles.content.analysis') }}</span> : {{ $content->analysis }}</p>
+        <p><span class="underline font-semibold">{{ __('titles.content.review') }}</span> : {{ $content->review }}</p>
+    </div>
 </div>
