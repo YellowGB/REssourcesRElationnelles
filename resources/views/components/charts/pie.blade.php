e5938b7ab66491b491b7a68153baa5ecc13cb13e
YellowGB <yellowchickencorp@gmail.com>
2022-06-23 21:09:58 +0200

    bugfix Livewire assets are out of date + creation CHANGELOG

--- resources/views/components/charts/pie.blade.php
+++ resources/views/components/charts/pie.blade.php
@@ -7,12 +7,13 @@
 <label>{{ $title }}</label>
 <div
     {{ $attributes->merge(['class' => '']) }}
+    {{-- &quot; car il n'est pas possible d'échaper le " dans ce contexte --}}
     x-data="{
         chart: null,
         setChart() {
             new Chartisan({
                 el: $el,
-                url: &quot;@chart($route, $request)&quot;, {{-- &quot; car il n'est pas possible d'échaper le " dans ce contexte --}}
+                url: &quot;@chart($route, $request)&quot;,
                 hooks: new ChartisanHooks()
                     .axis(false)
                     .tooltip()
