c33330c3f95013fd80c99452961c03bc7abce35a
YellowGB <yellowchickencorp@gmail.com>
2022-06-26 23:10:57 +0200

    chart menu animation

--- resources/views/components/charts/section.blade.php
+++ resources/views/components/charts/section.blade.php
@@ -1,4 +1,8 @@
 @props(['section' => ''])
-<div @click="hideAll(); {{ $section }} = true; showSections = false;" class="w-full border-y border-gris-normal hover:bg-primaire hover:text-blanc text-center">
+<div
+    @click="hideAll(); {{ $section }} = true; showSections = false;"
+    class="w-full border-y py-2 border-gris-normal hover:bg-primaire hover:text-blanc text-center"
+    :class="{ 'bg-primaire text-blanc': {{ $section }} == true }"
+>
     {{ $slot }}
 </div>
--- resources/views/livewire/stats-dashboard.blade.php
+++ resources/views/livewire/stats-dashboard.blade.php
@@ -16,13 +16,25 @@
 >
     {{-- Menu --}}
     <div x-data="{ showSections: false }" @click.away="showSections = false" class="w-full select-none">
-        <div @click="showSections = ! showSections" class="bg-primaire text-blanc border-y-2 border-y-primaire hover:border-y-slate-700 flex flex-col items-center justify-center">
+        <div @click="showSections = ! showSections" class="relative z-50 bg-primaire text-blanc border-y-2 border-y-primaire hover:border-y-slate-700 flex flex-col items-center justify-center">
             <div class="flex flex-col justify-center items-center">
                 <span class="text-blanc font-medium">@lang('titles.chart.section.menu')</span>
                 <x-icons.chevron-down class="w-6 h-6" />
             </div>
         </div>
-        <div x-show="showSections" class="bg-blanc flex flex-col items-center justify-center border border-primaire">
+        <div
+            x-show="showSections"
+
+            {{-- Slide animation vers le bas/haut --}}
+            x-transition:enter="ease-out duration-200"
+            x-transition:enter-start="h-16 border-none"
+            x-transition:enter-end="h-40 border-none"
+            x-transition:leave="ease-in duration-200"
+            x-transition:leave-start="h-40 border-none"
+            x-transition:leave-end="h-16 border-none"
+
+            class="absolute z-40 w-full bg-blanc flex flex-col items-center justify-center border border-primaire"
+        >
             <x-charts.section :section="'showSearches'">@lang('titles.chart.section.searches')</x-charts.section>
             <x-charts.section :section="'showProgress'">@lang('titles.chart.section.progress')</x-charts.section>
             <x-charts.section :section="'showUsers'">@lang('titles.chart.section.users')</x-charts.section>
