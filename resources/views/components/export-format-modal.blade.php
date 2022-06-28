c1d63712434439c1b5a71ff7d2fe6fc5b7fa8c4a
YellowGB <yellowchickencorp@gmail.com>
2022-06-28 14:16:05 +0200

    [UPDATE] export modal auto closes after download

--- resources/views/components/export-format-modal.blade.php
+++ resources/views/components/export-format-modal.blade.php
@@ -1,9 +1,9 @@
-<div>
-    <input type="checkbox" id="export-format-modal-{{ $route }}" class="modal-toggle" />
+<div x-data="{ checkbox: null }">
+    <input x-init="checkbox = $el" type="checkbox" id="export-format-modal-{{ $route }}" class="modal-toggle" />
     <label for="export-format-modal-{{ $route }}" class="modal cursor-pointer">
         <label class="modal-box relative bg-blanc dark:bg-gris-normal flex justify-around items-center" for="">
-            <a href="{{ route($export, ['format' => 'xlsx']) }}"><x-icons.xlsx /></a>
-            <a href="{{ route($export, ['format' => 'csv']) }}"><x-icons.csv /></a>
+            <a href="{{ route($export, ['format' => 'xlsx']) }}" @click="checkbox.checked = false"><x-icons.xlsx /></a>
+            <a href="{{ route($export, ['format' => 'csv']) }}" @click="checkbox.checked = false"><x-icons.csv /></a>
         </label>
     </label>
 </div>
